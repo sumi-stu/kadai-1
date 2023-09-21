d785b5852eccae5f444fc068e187f3dfe91ee176
asuka.y <emp.asu.0x0@gmail.com>
2023-09-21 15:29:26 +0000

    second commit, test.rb

--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
+
+=begin
 puts "Hello World!"
 
 today = "2023/9/21"
@@ -11,9 +13,42 @@ fruits.each do |fruit|
     puts x.to_s + " : " + fruit
     # puts fruit
 end
+=end
+
 
 LastName = 'Tanaka'
 FirstName = 'Taro'
 Age = 20
 
 puts "My name is " + LastName + FirstName + ". " + "Age..." + Age.to_s + "!"
+
+puts '----------------'
+
+number = 0
+#10.times do
+while number < 7
+    number = rand(10)
+    
+    if number < 6
+        puts number.to_s + 'は6より小さい数'
+    else
+        puts number.to_s + 'は6以上の数'
+    end
+end
+
+puts '----------------'
+
+# Create Method
+def sum(a, b)
+    result = 0
+    (a..b).each do |number|
+        result += number
+    end
+    puts result
+    # return result
+    # result
+end
+
+# puts の戻り値は nil
+puts sum(1, 10)
+
